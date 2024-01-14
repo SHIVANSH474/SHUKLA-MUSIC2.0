22eccf9e0e85064cc3760d43f970835706714756
SHIVANSH XD <149559899+SHIVANSH474@users.noreply.github.com>
2024-01-14 13:31:11 +0530

    Update afk.py

--- VIPMUSIC/plugins/bot/afk.py
+++ VIPMUSIC/plugins/bot/afk.py
@@ -8,7 +8,7 @@ from VIPMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 
-@app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
+@app.on_message(filters.command(["afk", "bye"], prefixes=["/", "!"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
         return
